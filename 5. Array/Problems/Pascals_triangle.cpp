930d9dc3d69908b754a78cf559b34f334a33a8e2
Chandra Sekhar Bala <57448981+Chandra-Sekhar-Bala@users.noreply.github.com>
2021-09-22 22:40:02 +0530

    Create Pascals_triangle.cpp

---
+++ 5. Array/Problems/Pascals_triangle.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int rows, coef = 1, space, i, j;
+	cout << "Enter the number of rows : ";
+	cin >> rows;
+	cout << endl;
+
+	for (i = 0; i < rows; i++)
+	{
+		for (space = 1; space <= rows - i; space++)
+			cout << " ";
+
+		for (j = 0; j <= i; j++)
+		{
+			if (j == 0 || i == 0)
+				coef = 1;
+			else
+				coef = coef *(i - j + 1) / j;
+			cout << coef << " ";
+		}
+
+		cout << endl;
+		cout << endl;
+	}
+
+	return 0;
+}
